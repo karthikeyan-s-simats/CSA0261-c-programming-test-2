f08d6fa0709bffc6bce4a0667c1a89dcbb0ba82d
karthikeyan-s-simats <129312612+karthikeyan-s-simats@users.noreply.github.com>
2023-03-30 09:22:18 +0530

    Add files via upload

---
+++ test 1.cpp
@@ -0,0 +1,43 @@
+#include<studio.h>
+#include<conio.h>
+int main[]
+{
+     int arr[50], i, j, flag;
+     printf(%d,n)
+    //initialize the array with numbers from 1 to 50
+    for (i = 0; i < 50; i++)
+        arr[i] = i + 1; 
+           int arr[50], i, j, flag;
+       }
+   
+    // Loop through the array to check for composite numbers
+    for (i = 0; i < 50; i++) {
+        // Skip checking if the number is 1 or a prime number
+        if the number is composite print it 
+     if (is_composite == 1) {
+            printf("%d ", arr[i]);
+        }
+    return 0;
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
---
+++ test 2.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h> 
+{
+ filter_string(first_string, second_string):
+    first_set = set(first_string)
+    result = ""
+    for characters in second_string:
+        if characters not in first_set:
+            result += character
+    return 
+}
+
